ddc3c300fd2d6a7b5f60021f09c82d8c136ace27
Tirthankar Kundu <tirthankar.kundu94@gmail.com>
2020-05-02 21:35:10 +0530

    Added Express Server with GraphQL for hard coded schema

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express"); // import express
+
+const app = express(); // initialize app
+
+const graphqlHTTP = require("express-graphql"); // import express-graphql for the interactive UI GraphiQL
+
+const postSchema = require("./schema"); // import the schema
+
+const PORT = 3000; // Hard coded port on which the server will run
+
+app.listen(PORT, () => {
+  console.log(`Server is up on ${PORT}`);
+});
+
+
+// Creates the interactive GraphiQL UI
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: postSchema,
+    graphiql: true,
+  })
+);
